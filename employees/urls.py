7d4fceb2db5825205740c67c840cefde6629da15
coherencedemo <164892412+coherencedemo@users.noreply.github.com>
2024-10-03 16:17:58 +0200

    Update urls.py with health check

--- employees/urls.py
+++ employees/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/health', views.health_check, name='health_check'),
+    path('health/', views.health_check, name='health_check'),
     path('edit/<int:pk>/', views.employee_edit, name='employee_edit'),
     path('delete/<int:pk>/', views.employee_delete, name='employee_delete'),
 ]
