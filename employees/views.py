589cec2f78a5087144447277fdabef905f87eac4
coherencedemo <164892412+coherencedemo@users.noreply.github.com>
2024-10-03 16:17:21 +0200

    Healthcheck update

--- employees/views.py
+++ employees/views.py
@@ -15,9 +15,8 @@ def index(request):
 	employees = Employee.objects.all()
 	return render(request, 'index.html', {'form': form, 'employees': employees})
 
-@app.route('/health')
-def health_check():
-    return 'OK', 200
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 def employee_edit(request, pk):
 	employee = get_object_or_404(Employee, pk=pk)
